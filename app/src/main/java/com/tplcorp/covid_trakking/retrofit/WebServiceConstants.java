970af351f74693bcaf360e77a3ed40799cb1e20c
Daniyal Ahmed Khan <daniyalahmed520@gmail.com>
2021-03-02 10:54:50 +0500

    Update WebServiceConstants.java

--- app/src/main/java/com/tplcorp/covid_trakking/retrofit/WebServiceConstants.java
+++ app/src/main/java/com/tplcorp/covid_trakking/retrofit/WebServiceConstants.java
@@ -4,15 +4,4 @@ public class WebServiceConstants {
 
 
 
-    public static  final String LIVE_COVID_STATS = "https://portal.tpllife.com/Covid19/";
-    public static final String LIVE_SERVER = "https://mytrakker.tpltrakker.com/Covid19/";
-
-    public static final String SERVER_URL = LIVE_SERVER;
-
-
-    public static final String KEY_USER_LOGIN = "SignIn";
-    public static final String KEY_POST_INTERACTIONS = "SendInteractions";
-    public static final String KEY_POST_COVID_STATS = "GetProvinceWiseCases";
-    public static final String CHECK_IS_INFECTED = "CheckIsInfected";
-    public static final String SUBMIT_REPORT = "SubmitReport";
 }
